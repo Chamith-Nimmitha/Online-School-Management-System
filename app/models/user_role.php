cff1f7def8384cdd9e24d8fc678395d080dc088d
= <chamith123.cn@gmail.com>
2020-12-03 11:21:16 +0530

    user_role mangement

--- app/models/user_role.php
+++ app/models/user_role.php
@@ -23,6 +23,42 @@
 			return $result_set->fetchAll();
 		}
 
+		// create userrole{
+		public function create_user_role($user_role_name){
+			$models = $this->get_models();
+
+			try {
+				$this->con->db->beginTransaction();
+				$result = $this->con->select("user_role",['name'=>$user_role_name]);
+				if(!$result || $result->rowCount() !== 0 ){
+					throw new PDOException();
+				}
+				$result = $this->con->insert("user_role",['name'=>$user_role_name]);
+				if(!$result || $result->rowCount() !== 1){
+					throw new PDOException();
+				}
+				$result = $this->con->select("user_role",['name'=>$user_role_name]);
+				if(!$result || $result->rowCount() !== 1 ){
+					throw new PDOException();
+				}
+				$user_role_id = $result->fetch()['id'];
+
+				foreach ($models as $model) {
+					$result = $this->con->insert("permission",["user_role_id"=>$user_role_id,"model_id"=>$model['id']]);
+					if(!$result || $result->rowCount() !== 1){
+						throw new PDOException();
+					}
+				}
+				$this->con->db->commit();
+				return $user_role_id;
+			} catch (Exception $e) {
+				$this->con->db->rollBack();
+				return FALSE;
+			}
+
+			
+		}
+
 		// get user permissions
 		public function get_permissions( $user_role_id ){
 			$query = "SELECT `p`.*,`ur`.`name` AS `user_role_name`,`m`.`name` AS `model_name`  FROM `permission` AS `p` JOIN `user_role` AS `ur` ON `p`.`user_role_id`=`ur`.`id` JOIN `model` AS `m` ON `p`.`model_id`=`m`.`id` WHERE `p`.`user_role_id`=$user_role_id";
@@ -33,6 +69,24 @@
 			return $result_set->fetchAll();
 
 		}
-	}
 
+		// update user permissions
+		public function update_permissions($data, $where){
+			try {
+				$this->con->db->beginTransaction();
+				foreach ($data as $model_id => $permissions) {
+					$where['model_id'] = $model_id;
+					$result = $this->con->update("permission",$permissions,$where);
+					if(!$result){
+						throw new PDOException("Update failed", 1);
+					}
+				}
+				$this->con->db->commit();
+				return TRUE;
+			} catch (Exception $e) {
+				$this->con->db->rollBack();
+				return FALSE;
+			}
+		}
+	}
  ?>
--- app/views/admin/user_role_all.php
+++ app/views/admin/user_role_all.php
@@ -4,24 +4,35 @@
 		<h2 class="fs-30">User Roles</h2>
 	</div>
 	<hr class="col-12 mt-5 mb-5">
-	<div class="col-12 d-flex flex-col align-items-center">
+	<div class="col-12">
+		<div class="d-flex col-12">
+			<form action="<?php echo set_url('userrole/permission'); ?>" method="POST" class="d-flex col-6 justify-content-center align-items-center">
+				<div class="form-group" style="width: 200px;">
+					<label for="user-role-id">User Role</label>
+					<select name="user-role-id" id="user-role-id">
+						<?php foreach ($user_roles as $role) {?>
+							<option value="<?php echo $role['id']; ?>"
+								 <?php if($user_role_id == $role['id']){echo "selected='selected'";}?> ><?php echo $role['name']; ?></option>
+						<?php } ?>
+					</select>
+				</div>
+				<div class="d-flex mt-3">
+					<button type="submit" class="btn btn-blue mt-5" name="submit" >Submit</button>
+				</div>
+			</form>
+			<form action="<?php echo set_url('userrole/create'); ?>" method="POST" class="d-flex col-6 justify-content-center align-items-center">
+				<div class="form-group col-6">
+					<label for="user-role-name">User Role Name</label>
+					<input type="text" name="user-role-name" id="user-role-name" placeholder="User Role Name">
+				</div>
+				<div class="d-flex mt-3">
+					<button type="submit" class="btn btn-blue mt-5" name="create" >Create</button>
+				</div>
+			</form>
+		</div>
 
-		<form action="<?php echo set_url('userrole/permission'); ?>" method="POST" class="d-flex col-12 justify-content-center align-items-center">
-			<div class="form-group" style="width: 200px;">
-				<label for="user-role-id">User Role</label>
-				<select name="user-role-id" id="user-role-id">
-					<?php foreach ($user_roles as $role) {?>
-						<option value="<?php echo $role['id']; ?>"
-							 <?php if($user_role_id == $role['id']){echo "selected='selected'";}?> ><?php echo $role['name']; ?></option>
-					<?php } ?>
-				</select>
-			</div>
-			<div class="d-flex mt-3">
-				<button type="submit" class="btn btn-blue mt-5" name="submit" >Submit</button>
-			</div>
-		</form>
-
-		<form action="user_role_all.php" method="POST" class="d-flex flex-col col-12 align-items-center">
+		<form action="<?php echo set_url('userrole/permission/update'); ?>" method="POST" class="d-flex flex-col col-12 align-items-center">
+			<input type="hidden" name="user-role-id" value="<?php echo $user_role_id ?>">
 			<div class="d-flex flex-col col-12" style="overflow-x: scroll;overflow-y: hidden;">
 				<table class='table-strip-dark  text-center'>
 					<thead>
@@ -38,10 +49,10 @@
 						foreach ($permissions as $permission) {?>
 							<tr>
 							 	<th><?php echo $permission['model_name']; ?></th>
-							 	<td><input type="checkbox" name="edit" value="1" <?php if($permission['edit'] == 1){echo "checked='checked'";	} ?>></td>
-							 	<td><input type="checkbox" name="view" value="1" <?php if($permission['view'] == 1){echo "checked='checked'";	} ?>></td>
-							 	<td><input type="checkbox" name="update" value="1" <?php if($permission['update'] == 1){echo "checked='checked'";	} ?>></td>
-							 	<td><input type="checkbox" name="delete" value="1" <?php if($permission['delete'] == 1){echo "checked='checked'";	} ?>></td>
+							 	<td><input type="checkbox" name="edit-<?php echo $permission['model_id'];?>" value="1" <?php if($permission['edit'] == 1){echo "checked='checked'";	} ?>></td>
+							 	<td><input type="checkbox" name="view-<?php echo $permission['model_id'];?>" value="1" <?php if($permission['view'] == 1){echo "checked='checked'";	} ?>></td>
+							 	<td><input type="checkbox" name="update-<?php echo $permission['model_id'];?>" value="1" <?php if($permission['update'] == 1){echo "checked='checked'";	} ?>></td>
+							 	<td><input type="checkbox" name="delete-<?php echo $permission['model_id'];?>" value="1" <?php if($permission['delete'] == 1){echo "checked='checked'";	} ?>></td>
 							</tr>
 				 	<?php }
 				 	} ?>
