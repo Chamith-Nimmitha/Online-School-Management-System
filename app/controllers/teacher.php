628887bc24db7224b54ba106df3cd3823f196ab0
Hemakanth <hkanth098@gmail.com>
2020-12-07 17:47:18 +0530

    teacher update and delete

--- app/controllers/teacher.php
+++ app/controllers/teacher.php
@@ -122,11 +122,124 @@
 		}
 
 		//update teacher details
-		public function teacher_list_update(){
+		public function update_teacher($teacher_id){
+
+
+			$con = new Database();
+			$field_errors = array();
+			$info = "";
+			$error = "";
+			
+			if(isset($_POST['update'])){
+
+				$name_with_initials = addslashes($_POST['name_with_initials']);
+				$first_name = addslashes($_POST['first_name']);
+				$middle_name = addslashes($_POST['middle_name']);
+				$last_name = addslashes($_POST['last_name']);
+				$gender = addslashes($_POST['gender']);
+				$dob = addslashes($_POST['dob']);
+				$address = addslashes($_POST['address']);
+				$email = addslashes($_POST['email']);
+				$nic = addslashes($_POST['nic']);
+
+			
+				if(!valid_email($email)){
+					$field_errors["email"] = "Invalid email address.";
+				}
+
+				$contact_number = addslashes($_POST['contact_number']);
+		
+
+				$required_fields = array();
+				$required_fields['name_with_initials']=[0,50,1,"Name with initials"];
+				$required_fields['first_name']=[0,20,1,"First name"];
+				$required_fields['middle_name']=[0,50,0,"Middle name"];
+				$required_fields['last_name']=[0,20,1,"Last name"];
+				$required_fields['gender']=[1,6,1,"Gender"];
+				$required_fields['dob']=[10,10,1,"Dath of birth"];
+				$required_fields['address']=[0,100,1,"Address"];
+				$required_fields['email']=[0,100,1,"Email"];
+				$required_fields['nic']=[10,12,1,"NIC"];
+				$required_fields['contact_number']=[10,10,1,"Contact Number"];
+
+				$c_result = validate_contact_number($_POST['contact_number']);
+				if($c_result !== 1){
+					$field_errors['contact-number'] = $c_result;
+				}
+
+				$field_errors = array_merge($field_errors,check_input_fields($required_fields));
+
+				if(empty($field_errors)){
+					$data = array();
+					$data['name_with_initials']=$name_with_initials;
+					$data['first_name']=$first_name;
+					$data['middle_name']=$middle_name;
+					$data['gender']=$gender;
+					$data['dob']=$dob;
+					$data['address']=$address;
+					$data['nic']=$nic;
+					$data['contact_number']=$contact_number;
+					$data['email']=$email;
+					$data['last_name']=$last_name;
+					
+
+					if(!isset($middle_name)){
+						$data['middle_name']="";
+					}
+
+				}
+
+				if(count($field_errors) === 0 ){
+						$con->db->beginTransaction();
+						try {
+								$result=$con->update("teacher",$data,array("id"=>$teacher_id));
+								if(!$result){
+									throw new PDOException("Failed to Update.", 1);
+								}else if(!$result || $result->rowCount() !== 1){
+									throw new PDOException("Nothing to update.", 1);
+								}
+
+								$con->db->commit();	
+						}catch (Exception $e) {
+							$con->db->rollback();
+							$error = $e->getMessage();
+						}
+
+					if(empty($error)){
+						$info="Updated Successfully";
+					}
+					
+
+				}else{
+					$error="Registration failed.";
+				}
+
+
+
+			}
+
+
+			$this->load->model("teacher");
+			$this->load->teacher->set_by_id($teacher_id);
+			$teacher_data=$this->load->teacher->get_data();
+			$data1['data'] = $teacher_data;
+
+			foreach ($teacher_data as $name => $value) {
+				$row[$name]=$value;
+			}
+
 
 			$this->view_header_and_aside();
-            $this->load->view("teacher/teacher_list_update");
+            $this->load->view("teacher/teacher_list_update",["data"=>$row,"field_errors"=>$field_errors,"info"=>$info,"error"=>$error]);
             $this->load->view("templates/footer");
+		}
+
+		public function delete($teacher_id){
+
+			$con=new Database();
+			$result=$con->delete('teacher',array('id'=>$teacher_id));
+
+			header("Location:". set_url("teacher/list"));
 		}
 	 }
 ?>
